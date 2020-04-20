a9b9463c63fa677b188207c9e22848e27704601b
Ernesto Stifano <ernesto.stifano@codistica.com>
2020-04-21 00:38:04 +0200

    [NEW] [*] - Implement @codistica/prettier-config-default package.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,3 @@
 'use strict';
 
-module.exports = {
-    printWidth: 80,
-    useTabs: false,
-    tabWidth: 4,
-    singleQuote: true,
-    trailingComma: 'none',
-    semi: true,
-    bracketSpacing: false,
-    arrowParens: 'always',
-    jsxSingleQuote: true,
-    jsxBracketSameLine: true,
-    endOfLine: 'auto'
-};
+module.exports = '@codistica/prettier-config-default';
