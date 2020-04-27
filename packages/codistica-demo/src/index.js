7edda6004bb3bbcca16d3046ea5fb354fb2a3d9d
Ernesto Stifano <ernesto.stifano@codistica.com>
2020-04-27 15:52:31 +0200

    [REFACTOR] [demo] - Further enforce project conventions.

--- packages/codistica-demo/src/index.js
+++ packages/codistica-demo/src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './app.js';
-import * as serviceWorker from './serviceWorker.js';
+import * as serviceWorker from './service-worker.js';
 
 ReactDOM.render(
     <React.StrictMode>
