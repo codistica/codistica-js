ebff7943ed209a8a4ba501fba0dab22959cdd09d
Ernesto Stifano <ernesto.stifano@codistica.com>
2021-05-25 17:26:35 +0200

    [FIX] [demo] - Adjust project to match CRA v4 template.

--- packages/codistica-demo/src/index.js
+++ packages/codistica-demo/src/index.js
@@ -4,7 +4,7 @@ import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {App} from './app.js';
-import * as serviceWorker from './service-worker.js';
+import {reportWebVitals} from './report-web-vitals.js';
 import './fonts/iceland-codistica/iceland-codistica.css';
 import './fonts/jura-codistica/jura-codistica.css';
 
@@ -20,7 +20,7 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
