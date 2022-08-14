9f42821898e32c701b4453cd2f8d52cd4be2ae7a
Ernesto Stifano <ernesto.stifano@codistica.com>
2022-08-14 11:04:08 +0200

    [NEW] [*] - Improve tests coverage configuration.

--- nyc.config.js
+++ nyc.config.js
@@ -1,1 +1,23 @@
-module.exports = {};
+module.exports = {
+    reporter: ['lcov', 'text'],
+    all: true,
+    extension: ['.js'],
+    include: [
+        'packages/codistica-core/lib',
+        'packages/codistica-dev-tools/lib',
+        'packages/codistica-node/lib',
+        'packages/codistica-scriptfiber/lib',
+        'packages/codistica-types/lib'
+    ],
+    'exclude-after-remap': false,
+    watermarks: {
+        lines: [80, 95],
+        functions: [80, 95],
+        branches: [80, 95],
+        statements: [80, 95]
+    },
+    branches: 80,
+    lines: 80,
+    functions: 80,
+    statements: 80
+};
